5f719f230cde89bd99da51da0ee7755c4087e349
Willian <wolverinewa@gmail.com>
2023-09-28 16:59:54 -0300

    2.4. Cadastrando um novo título

--- src/main/java/com/lilossystems/cobranca/controller/TituloController.java
+++ src/main/java/com/lilossystems/cobranca/controller/TituloController.java
@@ -1,15 +1,20 @@
 package com.lilossystems.cobranca.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.lilossystems.cobranca.model.Titulo;
+import com.lilossystems.cobranca.repository.Titulos;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
 
+	@Autowired
+	private Titulos titulos;
+	
 	@RequestMapping("/novo")
 	public String novo() {
 		return "CadastroTitulo";
@@ -18,7 +23,9 @@ public class TituloController {
 	@RequestMapping(method = RequestMethod.POST)
 	public String salvar(Titulo titulo) {
 		
-		System.out.println(">>> " + titulo.getDescricao());
+		//System.out.println(">>> " + titulo.getDescricao());
+		
+		titulos.save(titulo);
 		
 		// Retorna para uma VIEW
 		return "CadastroTitulo";
---
+++ src/main/java/com/lilossystems/cobranca/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.lilossystems.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lilossystems.cobranca.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long> {
+
+}
